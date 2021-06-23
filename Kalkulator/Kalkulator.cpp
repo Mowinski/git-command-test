79a3c16ef5046462448af78dad836bf771f57df2
AnnaGawrysiak <anna.m.gawrysiak@gmail.com>
2021-06-23 18:47:35 +0200

    Add math.h file

--- Kalkulator/Kalkulator.cpp
+++ Kalkulator/Kalkulator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "math.h"
 
 
 float add(int a, int b);
@@ -7,6 +8,11 @@ float add(int a, int b);
 int main()
 {
     std::cout << "Hello World!\n";
+
+    int a = 5;
+    int b = 8;
+
+    std::cout << a << " - " << b << " = " << substract(a, b) << std::endl;
 
     return 0;
 }
--- Kalkulator/math.cpp
+++ Kalkulator/math.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "math.h"
 
 float substract(int a, int b)
 {
